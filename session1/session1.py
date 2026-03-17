a1a67d7cdcaf76b631e731911fd18958285f2429
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-17 10:59:33 +0530

    added notes for session 1

---
+++ session1/session1.py
@@ -0,0 +1,8 @@
+my_int = 1
+my_float = 1.0
+my_string = "Hello again!"
+# ["H", "e", "l", "l", "o", " ", "a", "g", "a", "i", "n", "!"]
+# [ 0    1   2    3    4    5    6    7    8    9   10   11]
+
+print(my_string[::-1])
+print(my_string[5::-1])
